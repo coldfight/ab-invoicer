7349c38ebe449b796c2d0c6bdd8060530d3d24fb
Amedeo Bonanno <coldfight@live.com>
2022-10-21 14:48:18 -0400

    Add some demo of embedding image, css, using loop in go template

--- cmd/invoicer/main.go
+++ cmd/invoicer/main.go
@@ -1,358 +1,141 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
 	"fmt"
-	"github.com/johnfercher/maroto/pkg/color"
-	"github.com/johnfercher/maroto/pkg/consts"
-	"github.com/johnfercher/maroto/pkg/pdf"
-	"github.com/johnfercher/maroto/pkg/props"
+	"github.com/SebastiaanKlippert/go-wkhtmltopdf"
+	"html/template"
+	"log"
+	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
-var (
-	darkGrayColor color.Color = getDarkGrayColor()
-	grayColor     color.Color = getGrayColor()
-	whiteColor    color.Color = color.NewWhite()
-	blueColor     color.Color = getBlueColor()
-	redColor      color.Color = getRedColor()
-)
-
-func renderContactInfo(m pdf.Maroto) {
-	m.Text("Jane Doe", props.Text{
-		Size:        8,
-		Align:       consts.Left,
-		Extrapolate: false,
-		Color:       redColor,
-	})
-	m.Text("555 John Street", props.Text{
-		Top:         4,
-		Size:        8,
-		Align:       consts.Left,
-		Extrapolate: false,
-		Color:       redColor,
-	})
-	m.Text("Mississauga, ON, M4T2T1", props.Text{
-		Top:         7.5,
-		Size:        8,
-		Align:       consts.Left,
-		Extrapolate: false,
-		Color:       redColor,
-	})
-	m.Text("Phone: 416.555.0555", props.Text{
-		Top:   13,
-		Style: consts.BoldItalic,
-		Size:  8,
-		Align: consts.Left,
-		Color: blueColor,
-	})
-	m.Text("Email: jane.doe@gmail.com", props.Text{
-		Top:   16,
-		Style: consts.BoldItalic,
-		Size:  8,
-		Align: consts.Left,
-		Color: blueColor,
-	})
+type RequestPdf struct {
+	body string
 }
 
-func renderInvoiceDateAndNumber(m pdf.Maroto) {
-
-	m.Text("I N V O I C E", props.Text{
-		Style: consts.Bold,
-		Size:  26,
-		Align: consts.Right,
-	})
-
-	var fromTop float64 = 12
-
-	m.Text("INVOICE # 001 | DATE: 01/01/2022", props.Text{
-		Top:   fromTop,
-		Size:  8,
-		Align: consts.Right,
-		Style: consts.Bold,
-	})
+func NewRequestPdf(body string) *RequestPdf {
+	return &RequestPdf{
+		body: body,
+	}
 }
 
-func renderHeader(m pdf.Maroto) {
-	m.RegisterHeader(func() {
-		m.Row(22, func() {
-			m.Col(4, func() {
-				renderContactInfo(m)
-			})
-			m.ColSpace(3)
-			m.Col(5, func() {
-				renderInvoiceDateAndNumber(m)
-			})
-		})
-		m.Line(1.0, props.Line{
-			Color: color.Color{
-				Red:   0,
-				Green: 0,
-				Blue:  0,
-			},
-			Style: consts.Solid,
-			Width: 0.5,
-		})
-		m.Row(3, func() {})
-	})
-
+func (r *RequestPdf) ParseTemplate(templateFileName string, data any) error {
+	t, err := template.ParseFiles(templateFileName)
+	if err != nil {
+		return err
+	}
+	buf := new(bytes.Buffer)
+	if err = t.Execute(buf, data); err != nil {
+		return err
+	}
+	r.body = buf.String()
+	return nil
 }
 
-func renderFooter(m pdf.Maroto) {
-	m.RegisterFooter(func() {
-		m.Row(20, func() {
-			m.Col(12, func() {
-				m.Text("Phone: 416.555.0555", props.Text{
-					Top:   13,
-					Style: consts.BoldItalic,
-					Size:  8,
-					Align: consts.Left,
-					Color: blueColor,
-				})
-				m.Text("Email: jane.doe@gmail.com", props.Text{
-					Top:   16,
-					Style: consts.BoldItalic,
-					Size:  8,
-					Align: consts.Left,
-					Color: blueColor,
-				})
-			})
-		})
-	})
+func (r *RequestPdf) GeneratePdf(pdfPath string) (bool, error) {
+	t := time.Now().Unix()
+	fileName := fmt.Sprintf("storage/%s.html", strconv.FormatInt(int64(t), 10))
+	err := os.WriteFile(fileName, []byte(r.body), 0644)
+	if err != nil {
+		panic(err)
+	}
+	f, err := os.Open(fileName)
+	if f != nil {
+		defer f.Close()
+		//defer os.Remove(fileName)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pdfg, err := wkhtmltopdf.NewPDFGenerator()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pdfg.AddPage(wkhtmltopdf.NewPageReader(f))
+	pdfg.PageSize.Set(wkhtmltopdf.PageSizeA4)
+	pdfg.Dpi.Set(300)
+
+	err = pdfg.Create()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = pdfg.WriteFile(pdfPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return true, nil
 }
 
-func renderBilledTo(m pdf.Maroto) {
-	m.Row(25, func() {
-		m.Col(2, func() {
-			m.Text("Billed To:", props.Text{
-				Top:   8,
-				Style: consts.Bold,
-			})
-		})
-		m.Col(10, func() {
-			m.Text("Some person out there", props.Text{
-				Top:  1,
-				Size: 9,
-			})
-			m.Text("5555 Somekindof Rd", props.Text{
-				Top:  6,
-				Size: 9,
-			})
+func readBootstrapStylesheet() template.CSS {
+	data, err := os.ReadFile("./assets/bootstrap.css")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-			m.Text("Mississauga, ON, M4T2T1", props.Text{
-				Top:  11,
-				Size: 9,
-			})
-
-			m.Text("905.555.0555", props.Text{
-				Top:  16,
-				Size: 9,
-			})
-
-		})
-	})
+	return template.CSS(string(data))
 }
 
-func renderExpenses(m pdf.Maroto) {
-	header := getExpensesHeader()
-	contents := getExpensesContents()
-
-	m.SetBackgroundColor(darkGrayColor)
-
-	m.Row(7, func() {
-		m.Col(3, func() {
-			m.Text("Expenses", props.Text{
-				Top:   1.5,
-				Size:  9,
-				Style: consts.Bold,
-				Align: consts.Left,
-				Color: color.NewWhite(),
-			})
-		})
-		m.ColSpace(9)
-	})
-
-	m.SetBackgroundColor(whiteColor)
-
-	m.TableList(header, contents, props.TableList{
-		HeaderProp: props.TableListContent{
-			Size:      9,
-			GridSizes: []uint{2, 6, 2, 2},
-		},
-		ContentProp: props.TableListContent{
-			Size:      8,
-			GridSizes: []uint{2, 6, 2, 2},
-		},
-		Align:                consts.Left,
-		AlternatedBackground: &grayColor,
-		HeaderContentSpace:   1,
-		Line:                 false,
-	})
-
-	m.Row(20, func() {
-		m.ColSpace(7)
-		m.Col(2, func() {
-			m.Text("Total Expenses:", props.Text{
-				Top:   2,
-				Style: consts.Bold,
-				Size:  8,
-				Align: consts.Right,
-			})
-		})
-		m.Col(3, func() {
-			m.Text("R$ 2.567,00", props.Text{
-				Top:   2,
-				Style: consts.Bold,
-				Size:  8,
-				Align: consts.Center,
-			})
-		})
-	})
-
+func toBase64(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
 }
 
-func renderLabour(m pdf.Maroto) {
-	header := getLabourHeader()
-	contents := getLabourContents()
+func convertImageToBase64() template.URL {
+	// Read the entire file into a byte slice
+	bytes, err := os.ReadFile("./assets/images/invoicer.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	m.SetBackgroundColor(darkGrayColor)
+	var base64Encoding string
 
-	m.Row(7, func() {
-		m.Col(3, func() {
-			m.Text("Labour", props.Text{
-				Top:   1.5,
-				Size:  9,
-				Style: consts.Bold,
-				Align: consts.Center,
-				Color: color.NewWhite(),
-			})
-		})
-		m.ColSpace(9)
-	})
+	// Determine the content type of the image file
+	mimeType := http.DetectContentType(bytes)
 
-	m.SetBackgroundColor(whiteColor)
+	// Prepend the appropriate URI scheme header depending
+	// on the MIME type
+	switch mimeType {
+	case "image/jpeg":
+		base64Encoding += "data:image/jpeg;base64,"
+	case "image/png":
+		base64Encoding += "data:image/png;base64,"
+	}
 
-	m.TableList(header, contents, props.TableList{
-		HeaderProp: props.TableListContent{
-			Size:      9,
-			GridSizes: []uint{2, 8, 2},
-		},
-		ContentProp: props.TableListContent{
-			Size:      8,
-			GridSizes: []uint{2, 8, 2},
-		},
-		Align:                consts.Left,
-		AlternatedBackground: &grayColor,
-		HeaderContentSpace:   1,
-		Line:                 false,
-	})
+	// Append the base64 encoded output
+	base64Encoding += toBase64(bytes)
 
-	m.Row(20, func() {
-		m.ColSpace(7)
-		m.Col(2, func() {
-			m.Text("Total Labour:", props.Text{
-				Top:   2,
-				Style: consts.Bold,
-				Size:  8,
-				Align: consts.Right,
-			})
-		})
-		m.Col(3, func() {
-			m.Text("R$ 2.567,00", props.Text{
-				Top:   2,
-				Style: consts.Bold,
-				Size:  8,
-				Align: consts.Center,
-			})
-		})
-	})
-
+	return template.URL(base64Encoding)
 }
 
 func main() {
-	begin := time.Now()
+	r := NewRequestPdf("")
 
-	m := pdf.NewMaroto(consts.Portrait, consts.A4)
-	m.SetPageMargins(10, 15, 10)
-	//m.SetBorder(true)
+	templatePath := "./templates/test.html"
+	outPath := "./storage/pdf.pdf"
 
-	renderHeader(m)
-	renderFooter(m)
-	renderBilledTo(m)
-
-	renderExpenses(m)
-	renderLabour(m)
-
-	err := m.OutputFileAndClose("pdf/invoice001.pdf")
-	if err != nil {
-		fmt.Println("Could not save PDF:", err)
-		os.Exit(1)
+	templateData := struct {
+		Data                string
+		Expenses            []string
+		BootstrapStylesheet template.CSS
+		Logo                template.URL
+	}{
+		Data:                "data goes here",
+		Expenses:            []string{"Lysol Aerosol", "Windex Window Cleaner", "Toilet Bowl Cleaner"},
+		BootstrapStylesheet: readBootstrapStylesheet(),
+		Logo:                convertImageToBase64(),
 	}
 
-	end := time.Now()
-	fmt.Println(end.Sub(begin))
-}
-
-func getExpensesHeader() []string {
-	return []string{"Quantity", "Description", "Unit Price", "Amount"}
-}
-
-func getExpensesContents() [][]string {
-	return [][]string{
-		{"1", "Swamp", "$12.00", "$12.00"},
-		{"1", "Sorin, A Planeswalker", "$12.00", "$12.00"},
-		{"1", "Tassa", "$12.00", "$12.00"},
-		{"1", "Skinrender", "$12.00", "$12.00"},
-		{"1", "Island", "$12.00", "$12.00"},
-		{"1", "Mountain", "$12.00", "$12.00"},
-		{"1", "Plain", "$12.00", "$12.00"},
-	}
-}
-
-func getLabourHeader() []string {
-	return []string{"Date", "Description", "Amount"}
-}
-
-func getLabourContents() [][]string {
-	return [][]string{
-		{"Jan 1, 2022", "Swamp", "$12.00"},
-		{"Jan 1, 2022", "Sorin, A Planeswalker", "$12.00"},
-		{"Jan 1, 2022", "Tassa", "$12.00"},
-		{"Jan 1, 2022", "Skinrender", "$12.00"},
-		{"Jan 1, 2022", "Island", "$12.00"},
-		{"Jan 1, 2022", "Mountain", "$12.00"},
-		{"Jan 1, 2022", "Plain", "$12.00"},
-	}
-}
-
-func getDarkGrayColor() color.Color {
-	return color.Color{
-		Red:   55,
-		Green: 55,
-		Blue:  55,
-	}
-}
-
-func getGrayColor() color.Color {
-	return color.Color{
-		Red:   200,
-		Green: 200,
-		Blue:  200,
-	}
-}
-
-func getBlueColor() color.Color {
-	return color.Color{
-		Red:   10,
-		Green: 10,
-		Blue:  150,
-	}
-}
-
-func getRedColor() color.Color {
-	return color.Color{
-		Red:   150,
-		Green: 10,
-		Blue:  10,
+	if err := r.ParseTemplate(templatePath, templateData); err == nil {
+		ok, _ := r.GeneratePdf(outPath)
+		fmt.Println(ok, "pdf generated successfully")
+	} else {
+		fmt.Println(err)
 	}
 }
