5e5a96a8c2ec580bf7c1df182748089014ffc1f9
Amedeo Bonanno <coldfight@live.com>
2022-10-29 09:28:03 -0400

    Log to file

--- cmd/invoicer/main.go
+++ cmd/invoicer/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/coldfight/ab-invoicer/internal/services/db_service"
 	"github.com/coldfight/ab-invoicer/internal/ui/app"
 	_ "github.com/mattn/go-sqlite3"
+	"os"
 )
 
 func main() {
-	//dbService.CreateInitialDatabase()
+	if os.Getenv("HELP_DEBUG") != "" {
+		if f, err := tea.LogToFile("logs/debug.log", "app"); err != nil {
+			fmt.Println("Couldn't open a file for logging:", err)
+			os.Exit(1)
+		} else {
+			defer f.Close()
+		}
+	}
+
+	db_service.CreateInitialDatabase()
 	//invoice := invoiceService.GetFullInvoiceRecord(1)
 	//invoiceService.NewDocumentFromInvoice(invoice)
 
